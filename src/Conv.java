6db01dd6b211ae600e1628f8f92da7c174ee8fb5
Vika94 <vikuscha1994@mail.ru>
2021-06-19 19:18:46 +0300

    пробелы

--- src/Conv.java
+++ src/Conv.java
@@ -1,12 +1,13 @@
 import java.util.Scanner;
+
 public class Conv {
     public static void main(String[] args) {
         System.out.print("Введите белорусский рубль: ");
         Scanner scan = new Scanner(System.in);
         double blr = scan.nextDouble();
         double kof = 0.398406374501992;
-        double usd = blr*kof;
-        System.out.println ("Получаем " + usd+ "$");
+        double usd = blr * kof;
+        System.out.println("Получаем " + usd + "$");
         scan.close();
     }
 }
