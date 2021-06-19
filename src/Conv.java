f401e215fe3f36ef34f0c5989fdaebbb3fc90dd2
Vika94 <vikuscha1994@mail.ru>
2021-06-19 18:51:14 +0300

    Initial commit

---
+++ src/Conv.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Conv {
+    public static void main(String[] args) {
+        System.out.print("Введите белорусский рубль: ");
+        Scanner scan = new Scanner(System.in);
+        double blr = scan.nextDouble();
+        double kof = 0.398406374501992;
+        double usd = blr*kof;
+        System.out.println ("Получаем " + usd+ "$");
+        scan.close();
+    }
+}
